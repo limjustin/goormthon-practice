8d537fe3bb7a216874df8572467862141d11a5ee
Jaeyoung Lim <limjyjustin@gmail.com>
2024-05-19 13:48:44 +0900

    Add files via upload

---
+++ src/test/java/dev/limjustin/jejupractice/JejupracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.limjustin.jejupractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejupracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
